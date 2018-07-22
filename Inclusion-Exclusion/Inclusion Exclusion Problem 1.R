bfebf55444ec3212762f30034e8769d00d5f7be0
Goutham Swaminathan <goutham1220@gmail.com>
2018-07-22 17:17:37 -0400

    added new files

--- Inclusion-Exclusion/Inclusion Exclusion Problem 1.R
+++ Inclusion-Exclusion/Inclusion Exclusion Problem 1.R
@@ -1,24 +1,26 @@
-numIterations = 10000
+library("plot3D")
+library(reshape2)
+library("plot3Drgl")
+library("gganimate")
+library(rgl)
+
+
+numIterations = 1000
 numPeople = 7
 
 simulation = function(nIterations, nPeople){
 
   seasons = matrix(data = 1:364, nrow = 91, ncol = 4)
   results = matrix(data = NA, nrow = nIterations, ncol = ncol(seasons))
-  seasonSums = vector(length = 4)
-  
+
   for(j in 1:nIterations){
 
-    people = matrix(data = sample(1:365, nPeople), nrow = 1, ncol = nPeople)
+    people = matrix(data = sample(1:364, nPeople, replace = TRUE), nrow = 1, ncol = nPeople)
     seasonalBirthdays = matrix(data = NA, nrow = ncol(seasons), ncol = ncol(people))
 
-    for(i in 1:ncol(seasons))
-      seasonalBirthdays[i,] = apply(people, MARGIN = 2, FUN = function(x) x %in% seasons[,i]) 
-
-    for(i in 1:nrow(seasonalBirthdays))
-      seasonSums[i] = sum(seasonalBirthdays[i,])
-
-    results[j,] = seasonSums
+    seasonalBirthdays = sapply(people, function(x) which(seasons == x, arr.ind = TRUE))[2,]
+    
+    results[j,] = as.vector(table(factor(seasonalBirthdays, levels = c(1, 2, 3, 4))))
   }  
    return(results)
 }
@@ -29,4 +31,19 @@ boolResults = apply(results[,1:4], MARGIN = 2, function(x) x >= 1)
 
 prob = sum(apply(boolResults, MARGIN = 1, all))/numIterations
 prob
-    
+
+colnames(results) = c("winter", "spring", "summer", "fall")
+results = data.frame(results)
+results$id = 1:numIterations
+results.melt = melt(results, id = "id")
+
+
+x = 1:4
+y = 1:numIterations
+z = results
+
+scatter3D(x=as.numeric(factor(results.melt$variable)), y=results.melt$id, results.melt$value, phi = 0, bty = "g",  type = "h", 
+          ticktype = "detailed", pch = 19, cex = 0.5)
+
+
+heatmap(results[,-c("id")])
